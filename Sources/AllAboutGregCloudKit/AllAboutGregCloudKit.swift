fc15d0dfa9fc70d62bcaeefdf973a389cc650d7f
Greg PFISTER <no-reply@mail.com>
2021-05-15 16:27:53 +0200

    [FIX] Public visibility

--- Sources/AllAboutGregCloudKit/AllAboutGregCloudKit.swift
+++ Sources/AllAboutGregCloudKit/AllAboutGregCloudKit.swift
@@ -4,6 +4,6 @@
 // Created by Greg PFISTER on 14/05/2021
 // Copyright © 2021, Greg PFISTER. All Rights Reserverd
 
-struct AllAboutGregCloudKit {
-    static var version: String = "0.0.1"
+public struct AllAboutGregCloudKit {
+    public static var version: String = "0.0.1"
 }

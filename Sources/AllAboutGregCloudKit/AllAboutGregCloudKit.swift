71ebdf8cb62d3c67c9b652f1c2f7ab94777280f7
Greg PFISTER <no-reply@mail.com>
2021-05-15 16:01:32 +0200

    [INFO] First commit

---
+++ Sources/AllAboutGregCloudKit/AllAboutGregCloudKit.swift
@@ -0,0 +1,9 @@
+//
+// AllAboutGregCloutKit
+//
+// Created by Greg PFISTER on 14/05/2021
+// Copyright © 2021, Greg PFISTER. All Rights Reserverd
+
+struct AllAboutGregCloudKit {
+    static var version: String = "0.0.1"
+}
